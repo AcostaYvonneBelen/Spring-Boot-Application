9ad725dc00a41f4d1994c2c07338682bd19f2a8e
Yvonne2021 <yvonne_belen_acosta@hotmail.com>
2021-01-12 19:10:00 -0300

    commit inicial con eclipse. Close #1

---
+++ Aplication_v1/src/main/java/com/example/demo/AplicationV1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicationV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicationV1Application.class, args);
+	}
+
+}
